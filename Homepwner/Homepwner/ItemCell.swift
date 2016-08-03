fc0f4bcb0742c0ba7afa7e9fa0f7867770424c81
helloyako <helloyako@gmail.com>
2016-08-03 10:38:15 +0900

    Complete chapter 11

---
+++ Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,25 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by helloyako on 2016. 8. 2..
+//  Copyright © 2016년 helloyako. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    func updateLabels() {
+        let bodyFont = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
+        nameLabel.font = bodyFont
+        valueLabel.font = bodyFont
+        
+        let caption1Font = UIFont.preferredFontForTextStyle(UIFontTextStyleCaption1)
+        serialNumberLabel.font = caption1Font
+    }
+    
+}
--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -20,6 +20,9 @@ class ItemsViewController: UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 65
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,14 +44,16 @@ class ItemsViewController: UITableViewController {
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("ItemCell", forIndexPath: indexPath) as! ItemCell
 
-        // Configure the cell...
         let item = itemStore.allItems[indexPath.row]
         
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-
+        cell.updateConstraints()
+        
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.valueInDollars)"
+        
         return cell
     }
     

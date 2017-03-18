c04a9b89483e86e117e371ad6060f98403f594a3
helloyako <helloyako@gmail.com>
2017-03-18 18:34:06 +0900

    Chapter 3 Complete

---
+++ 6th/WroldTrotter/WroldTrotter/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WroldTrotter
+//
+//  Created by helloyako on 2017. 3. 18..
+//  Copyright © 2017년 helloyako. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+
+}
+

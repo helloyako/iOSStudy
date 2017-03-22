2d51d3a32b5b4d5f88fdbb220f40d01205a0d045
helloyako <helloyako@gmail.com>
2017-03-22 18:02:52 +0900

    Silver Challenge in 5 chapter

--- 6th/WroldTrotter/WroldTrotter/ConversionViewController.swift
+++ 6th/WroldTrotter/WroldTrotter/ConversionViewController.swift
@@ -57,6 +57,29 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
         print("ConversionViewController loaded its view.")
         
     }
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let now = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH"
+        guard let hour: Int = Int(dateFormatter.string(from: now)) else {
+            view.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+            return
+        }
+        
+        switch hour {
+        case 0...6:
+            fallthrough
+        case 18...24:
+            view.backgroundColor = #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)
+        default:
+            view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 
 
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {

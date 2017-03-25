cee99cf368376f6088660776ad9401110f1e7055
helloyako <helloyako@gmail.com>
2017-03-25 15:52:53 +0900

    Complete Chapter 6

--- 6th/WroldTrotter/WroldTrotter/MapViewController.swift
+++ 6th/WroldTrotter/WroldTrotter/MapViewController.swift
@@ -7,8 +7,47 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: #selector(MapViewController.mapTypeChanged(_:)), for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        let margin = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margin.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margin.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+    }
+    
+    func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
